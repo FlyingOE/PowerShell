23e0fc013f89340d66e6e91ac4773de2b49e6765
Freddie Wu <freddie.wu@capitaledge.cn>
2019-02-15 15:20:58 +0800

    Get-ScriptDirectory

---
+++ Get-ScriptDirectory.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Obtain the directory of the current script.
+.LINK
+    https://gallery.technet.microsoft.com/scriptcenter/Get-ScriptDirectory-cbc423e1
+#>
+Function Get-ScriptDirectory {
+    If ($hostinvocation -ne $null) {
+        Split-Path $hostinvocation.MyCommand.Path
+    } else {
+        Split-Path $Script:MyInvocation.MyCommand.Path
+    }
+}
